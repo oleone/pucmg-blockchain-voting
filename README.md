8958d4aad0ca3a29920eb5173ccb4ffe38e50177
Bruno Leone A P Corrêa <gerencia@acetime.com.br>
2022-10-25 22:18:02 -0300

    Add heroku pipeline

---
+++ README.md
@@ -0,0 +1,7 @@
+<!-- https://devcenter.heroku.com/articles/build-docker-images-heroku-yml -->
+
+```bash
+heroku stack:set container --app pucmg-blockchain-voting
+```
+
+pucmg-blockchain-voting
--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,3 +1,5 @@
+const Web3 = require("web3");
+
 const tableElem = document.getElementById("table-body");
 const candidateOptions = document.getElementById("candidate-options");
 const voteForm = document.getElementById("vote-form");
