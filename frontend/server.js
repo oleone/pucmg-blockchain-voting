7fd37eb6613e693c4817e278aef69eefeb057907
Bruno Leone A P Corrêa <gerencia@acetime.com.br>
2022-10-26 00:10:22 -0300

    Add port

--- frontend/server.js
+++ frontend/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 80;
+
 app.get('/', function (req, res) {
     res.send({ ok: true }).status(200);
 });
 
-app.listen(8080, () => console.log(`Listening in port ${8080}`));
+app.listen(PORT, () => console.log(`Listening in port ${PORT}`));
