c559647a538261498c79c157c4f4bb8ddb180cea
Bruno Leone A P Corrêa <gerencia@acetime.com.br>
2022-10-25 22:56:02 -0300

    Update run apache2

--- frontend/run-apache2.sh
+++ frontend/run-apache2.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+# https://github.com/docker-library/wordpress/issues/293
 sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
-apache2-foreground "$@"
+/usr/local/bin/docker-entrypoint.sh apache2-foreground
