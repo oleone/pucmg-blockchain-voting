3c64375c06acb6115edb19a968b22e5b22c2943b
Bruno Leone A P Corrêa <gerencia@acetime.com.br>
2022-10-26 00:21:28 -0300

    Add public files

--- frontend/server.js
+++ frontend/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const PORT = process.env.PORT || 80;
 
-app.get('/', function (req, res) {
-    res.send({ ok: true }).status(200);
-});
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+// app.get('/', function (req, res) {
+//     res.send({ ok: true }).status(200);
+// });
 
 app.listen(PORT, () => console.log(`Listening in port ${PORT}`));
